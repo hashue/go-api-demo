1e759d8773f8ce9cb212577a97dbbd202af056ee
Hasu <hshue83@gmail.com>
2021-11-05 18:27:08 +0900

    [feat] add SIGINT handler and more...

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module go-api-demo
 
 go 1.17
-
-require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,27 +1,47 @@
 package main
 
 import (
-    "github.com/gorilla/mux"
     "net/http"
-    "log"
     "fmt"
+    "os"
+    "os/signal"
 )
 
 func getHello(w http.ResponseWriter, r *http.Request) {
+
+   // Varidate Method
+   if r.Method != http.MethodGet{
+       w.WriteHeader(http.StatusMethodNotAllowed)
+       w.Write([]byte("This api support only GET method"))
+       return
+   }
+
    w.WriteHeader(http.StatusOK)
    fmt.Fprintf(w, "hello!") //Fprintf can specify writer as parameter.
+}
+
+func siginthandler(){
+
+    fmt.Println("\nStop server...")
+
 }
 
 func main() {
     var port string = ":8000"
 
-    //initialize of router
-    r := mux.NewRouter()
-
     //routing
-    r.HandleFunc("/api/hello",getHello).Methods("GET")
+    http.HandleFunc("/api/hello",getHello)
 
     fmt.Printf("Listening on localhost%s...", port)
     
-    log.Fatal(http.ListenAndServe(port,r))
+    go http.ListenAndServe(port,nil)
+
+    quit := make(chan os.Signal)
+
+    signal.Notify(quit, os.Interrupt)
+
+    <-quit
+
+    siginthandler();
+
 }
